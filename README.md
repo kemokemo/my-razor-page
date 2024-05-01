d06ec1a36589de13d53f8327f23a98c021641406
kemokemo <t2wonderland@gmail.com>
2024-05-01 12:46:43 +0900

    fix: resharper issues (#2)
    * fix: resharper issues
    
    * docs: only warning and error severities

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # my-razor-page
+
 my razor page practice
+
+## Inspect code
+
+### Preparation
+
+```sh
+dotnet tool install -g JetBrains.ReSharper.GlobalTools
+```
+
+### Usage
+
+```sh
+jb inspectcode -e=WARNING -o="results-local.sarif" my-razor-page.sln
+```
--- my-razor-page/Pages/Index.cshtml.cs
+++ my-razor-page/Pages/Index.cshtml.cs
@@ -1,19 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace my_razor_page.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<IndexModel> _logger = logger;
 
     public void OnGet()
     {
-
+        _logger.LogInformation("OnGet was called.");
     }
 }
